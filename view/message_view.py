0247751edcf3650a0c3d6c4978eaad543dc82e1d
KinitoSs <66869636+KinitoSs@users.noreply.github.com>
2022-11-14 12:03:05 +0300

    основной каркас приложения

---
+++ view/message_view.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QDialog, QPushButton, QLabel
+from PyQt5 import uic
+
+
+class MessageView(QDialog):
+    __confirm_button: QPushButton
+    __message_label: QLabel
+
+    def __init__(self, label_text=None):
+        super().__init__()
+        uic.loadUi("UI/message_view.ui", self)
+        self.__setup_ui(
+            confirm_button=self.confirmButton, message_label=self.messageLabel
+        )
+        self.__message_label.setText(label_text)
+        self.__confirm_button.clicked.connect(lambda: self.close())
+
+    def __setup_ui(
+        self, confirm_button: QPushButton = None, message_label: QLabel = None
+    ):
+        self.__confirm_button = confirm_button
+        self.__message_label = message_label
--- view/tetris_view.py
+++ view/tetris_view.py
@@ -1,15 +1,35 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton
 from base.IMainWindow import IMainWindow
 
 
 class TetrisView(QWidget):
-    def __init__(self, main_window: IMainWindow = None) -> None:
-        super().__init__()
-        self.main_window = main_window
-        uic.loadUi("UI/tetris_view.ui", self)
-        self.toMenuViewButton.clicked.connect(self.to_menu_view)
+    __lvl: int
+    __main_window: IMainWindow
+    __label: QLabel
+    __to_menu_view_button: QPushButton
 
-    def to_menu_view(self):
-        self.main_window.go_to_previous_view()
-        self.main_window.remove_main_view(self)
+    def __init__(self, main_window: IMainWindow = None, lvl: int = 1) -> None:
+        super().__init__()
+        self.__lvl = lvl
+        self.__main_window = main_window
+        uic.loadUi("UI/tetris_view.ui", self)
+        self.__setup_ui(label=self.label, to_menu_view_button=self.toMenuViewButton)
+        self.__setup_labels()
+        self.__setup_buttons()
+
+    def __setup_ui(
+        self, label: QLabel = None, to_menu_view_button: QPushButton = None
+    ) -> None:
+        self.__label = label
+        self.__to_menu_view_button = to_menu_view_button
+
+    def __setup_labels(self) -> None:
+        self.__label.setText(f"Тетрис! Уровень {self.__lvl}")
+
+    def __setup_buttons(self) -> None:
+        self.__to_menu_view_button.clicked.connect(self.__to_menu_view)
+
+    def __to_menu_view(self) -> None:
+        self.__main_window.go_to_previous_view()
+        self.__main_window.remove_main_view(self)
